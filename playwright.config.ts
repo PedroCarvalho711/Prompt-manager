2dddd7670908204e3bfe6c87702607f80ddefc36
Pedro Carvalho <pedrocarvalh711@gmail.com>
2026-02-12 10:05:48 +0000

    Configurando o playwright para o primeiro teste e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ const PORT = process.env.PORT ?? '3000';
 const BASE_URL = `http://localhost:${PORT}`;
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
